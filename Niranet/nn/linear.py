5b04f6ce24066061b857acada043b77950980698
niranth-474 <niranthanandachar474@gmail.com>
2026-07-03 11:23:51 +0530

    Implemented linear.py and activation function which incluedes relu ,sigmoid ,tanH,LeakyReLU

---
+++ Niranet/nn/linear.py
@@ -0,0 +1,29 @@
+'''
+Linear Layer
+
+Definition
+
+A Linear Layer is a trainable neural network layer that performs a linear transformation on the input.
+
+Mathematically,  z=xw+b, where w is the weight matrix, x is the input vector, and b is the bias vector.
+
+'''
+
+
+
+import numpy as np
+from .module import Module
+
+class Linear(Module):
+
+    def __init__(self, input_size, output_size):
+        self.W = np.random.randn(input_size, output_size)
+        self.b = np.zeros((1, output_size))
+        self.X = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, X):
+        self.X = X
+        z = X @ self.W + self.b
+        return z
